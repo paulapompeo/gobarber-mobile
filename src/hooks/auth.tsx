28edf2d3b05bd5710790d5f814ac1f44d4167457
Paula Pompeo de Camargo Ranazzi <paulapompeo@ibm.com>
2020-09-10 18:41:42 -0300

    modulo finalizado

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -22,6 +22,7 @@ interface SignInCredentials {
 interface AuthContextData {
   // eslint-disable-next-line @typescript-eslint/ban-types
   user: object;
+  loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -33,6 +34,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 // ex: Login e Logout
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStoragedData(): Promise<void> {
@@ -44,6 +46,7 @@ const AuthProvider: React.FC = ({ children }) => {
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
+      setLoading(false);
     }
 
     loadStoragedData();
@@ -66,13 +69,13 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   const signOut = useCallback(async () => {
-    await AsyncStorage.multiRemove(['@GoBarber:token', '@GoBarber:user']);
+    await AsyncStorage.multiRemove(['@GoBarber:user', '@GoBarber:token']);
 
     setData({} as AuthState);
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
